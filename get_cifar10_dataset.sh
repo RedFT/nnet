800b05da2b4dc3f3a9fd3822325779b257e22a8e
Norbu Tsering <ntsering91@gmail.com>
2016-06-30 12:24:40 -0400

    added script to get CIFAR10 dataset

---
+++ get_cifar10_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Downloads and extracts the CIFAR-10 dataset
+# After this script finishes executing, you can run dmgen.py [num_frames]
+
+cifar10_link='https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+
+wget $cifar10_link
+
+cifar10_archive=$(basename $cifar10_link)
+
+unzip $cifar10_archive
+
+rm $cifar10_archive -rf
+
+`mv cifar-10-python data/cifar-10`
