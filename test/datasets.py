51b7504833fd8ccdaf1aacf7a34c85b1036f4a8e
Norbu Tsering <ntsering91@gmail.com>
2016-06-30 11:58:14 -0400

    added code for retrieving cifar10 dataset and preprocessing code

--- test/datasets.py
+++ test/datasets.py
@@ -4,26 +4,34 @@ import numpy as np
 def unpickle(file):
     import pickle
     fo = open(file, 'rb')
-    dict = pickle.load(fo, encoding='latin1')
+    data_dict = pickle.load(fo, encoding='latin1')
     fo.close()
-    return dict
+    return data_dict
 
 
-def get_cifar10():
-    data = []
-    for i in range(1, 6):
-        data.append(unpickle("data/cifar-10/data_batch_" + str(i)))
-
-    images = np.concatenate([item["data"] for item in data])
-    labels = np.concatenate([item["labels"] for item in data])
-
+def get_cifar10_dataset():
+    # load data
+    data = [unpickle("data/cifar-10/data_batch_" + str(i)) for i in range(1, 6)]
     testing_data = unpickle("data/cifar-10/test_batch")
     label_names = unpickle("data/cifar-10/batches.meta")
 
-    return images, labels, testing_data["data"], np.array(testing_data["labels"]), label_names["label_names"]
+    # combine all training data
+    training_images = np.concatenate([item["data"] for item in data])
+    training_labels = np.concatenate([item["labels"] for item in data])
+
+    # rename for readability
+    testing_images = testing_data["data"]
+    testing_labels = np.array(testing_data["labels"])
+    label_strings = label_names["label_names"]
+
+    return training_images, training_labels, testing_images, testing_labels, label_strings
 
 
-if __name__ == "__main__":
-    dataset = get_cifar10()
-    for item in dataset:
-        print(item.shape)
+def normalize(X):
+    return X.astype(np.float32) / 255
+
+
+def append_one(X):
+    ones_1 = np.array([np.ones([X.shape[0]])])
+    return np.append(X, ones_1.T, axis=1)
+
