ded3550282db76b53dd50f192bb20e3e0916cb89
Norbu Tsering <ntsering91@gmail.com>
2016-07-06 13:49:31 -0400

    Changed link in cifar10_dataset retrieval script

--- test/get_cifar10_dataset.sh
+++ test/get_cifar10_dataset.sh
@@ -3,8 +3,7 @@
 # Downloads and extracts the CIFAR-10 dataset
 # After this script finishes executing, you can run dmgen.py [num_frames]
 
-cifar10_link='https://www.cs.toronto.edu/~kriz/cifar-10-batches-py-python.tar.gz'
-
+cifar10_link="https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 wget $cifar10_link
 
 cifar10_archive=$(basename $cifar10_link)
